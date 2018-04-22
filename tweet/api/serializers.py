2262607965b4e0a02eb193c1eaed0a4d7e9bc276
JMSajib <jm.sajib012@gmail.com>
2018-04-22 15:50:38 +0600

    Added accounts app with serializer

--- tweet/api/serializers.py
+++ tweet/api/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from tweet.models import Tweet
+from accounts.api.serializers import UserDisplaySrializser
 
 class TweetModelSerializer(serializers.ModelSerializer):
+    user = UserDisplaySrializser()
     class Meta():
         model = Tweet
         fields = [
--- tweetme/settings/production.py
+++ tweetme/settings/production.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     # 'django.contrib.staticfiles',
+    'accounts',
     'tweet',
     'rest_framework',
     'crispy_forms',
