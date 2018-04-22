5f095f632fcbc2ca8feb693407dd7110bee6c34a
JMSajib <jm.sajib012@gmail.com>
2018-04-23 00:51:00 +0600

    Fixed list view issue

--- tweet/api/serializers.py
+++ tweet/api/serializers.py
@@ -4,9 +4,15 @@ from accounts.api.serializers import UserDisplaySrializser
 
 class TweetModelSerializer(serializers.ModelSerializer):
     user = UserDisplaySrializser(read_only=True)
+    date_display = serializers.SerializerMethodField()
     class Meta():
         model = Tweet
         fields = [
             'user',
-            'content'
+            'content',
+            'timestamp',
+            'date_display'
         ]
+
+    def get_date_display(self,obj):
+        return obj.timestamp.strftime(" %b %d, %Y | at %I:%M %p")    
