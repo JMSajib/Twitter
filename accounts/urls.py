d403287146d69855b203e20c2d186b7e810494f2
JMSajib <jm.sajib012@gmail.com>
2018-04-24 02:31:28 +0600

    Added Follow & Unfollow option

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
-from .views import UserDetailView
+from .views import UserDetailView,UserFollowView
 
 urlpatterns = [
     # url(r'^$', TweetListView.as_view(),name="list"),
     # url(r'^create/', TweetCreateView.as_view(),name="create"),
     url(r'^(?P<username>[\w.@+-]+)/$',UserDetailView.as_view(),name='detail'),
+    url(r'^(?P<username>[\w.@+-]+)/follow/$',UserFollowView.as_view(),name='follow'),
     # url(r'^update/(?P<pk>\d+)$',TweetUpdateView.as_view(),name='update'),
     # url(r'^delete/(?P<pk>\d+)$',TweetDeleteView.as_view(),name='delete'),
 ]
