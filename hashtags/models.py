4da85e25313c64487bdf67a1756c67d0da0c3d7f
JMSajib <jm.sajib012@gmail.com>
2018-04-26 01:35:08 +0600

    Created Signal for Hashtags

--- hashtags/models.py
+++ hashtags/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse_lazy
 from tweet.models import Tweet
+from .signals import parsed_hashtags
 # Create your models here.
 
 class HashTag(models.Model):
@@ -15,3 +16,10 @@ class HashTag(models.Model):
 
     def get_tweets(self):
         return Tweet.objects.filter(content__icontains="#" + self.tag)
+
+def parsed_hashtags_receiver(sender,hashtag_list,*args,**kwargs):
+    if(len(hashtag_list)>0):
+        for tag_var in hashtag_list:
+            new_tag, create = HashTag.objects.get_or_create(tag=tag_var)
+
+parsed_hashtags.connect(parsed_hashtags_receiver)
--- tweet/models.py
+++ tweet/models.py
@@ -1,7 +1,10 @@
+import re
+from django.db.models.signals import post_save
 from django.db import models
 from django.urls import reverse
 from .validators import validate_content
 from django.conf import settings
+from hashtags.signals import parsed_hashtags
 # Create your models here.
 
 class TweetManager(models.Manager):
@@ -44,3 +47,14 @@ class Tweet(models.Model):
     #     if(content == "abc"):
     #         raise ValidationError("Content cant be ABC")
     #     return super(Tweet,self).clean(*args,**kwargs)
+
+def tweet_save_receiver(sender,instance,created,*args,**kwargs):
+    if(created and not instance.parent):
+        user_regex = r'@(?P<username>[\w.@+-]+)'
+        username = re.findall(user_regex,instance.content)
+
+        hash_regex = r'#(?P<hashtags>[\w\d]+)'
+        hashtags = re.findall(hash_regex,instance.content)
+        parsed_hashtags.send(sender=instance.__class__, hashtag_list=hashtags)
+
+post_save.connect(tweet_save_receiver,sender=Tweet)
